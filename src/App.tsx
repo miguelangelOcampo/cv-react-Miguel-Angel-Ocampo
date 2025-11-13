e2c66fb1f2f19ce031cbaad8d0d3674e6c282307
Miguel Angel Ocampo Muñoz <miguelangelocampo713@gmail.com>
2025-11-13 08:06:15 -0500

    Creacion de componente de foto de perfil y restructuración de la hoja de vida con App.css

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,26 @@ import Cabecera from './Cabecera'
 import Estudios from './Estudios'
 import Experiencia from './Experiencia'
 import Perfil from './Perfil'
+import FotoPerfil from './foto_perfil'
+import './App.css'
 function App() {
   
 
   return (
     <>
-      <Cabecera />
-      <Perfil />
-      <Experiencia />
-      <Estudios />
+       <div className="cv-container">
+      <div className="left-section">
+        <FotoPerfil />
+        <Cabecera />
+        <Perfil />
+      </div>
+
+      <div className="right-section">
+        <Experiencia />
+        <Estudios />
+      </div>
+    </div>
+  
     </>
   )
 }
---
+++ src/foto_perfil.tsx
@@ -0,0 +1,12 @@
+export default function FotoPerfil() {
+    return (
+        <div className="inline-block p-2 rounded-full bg-gradient-to-r from-gray-900 via-gray-800 to-gray-900 shadow-lg shadow-black/40 border-4 border-blue-400">
+  <img
+    src="/img/foto.jpeg"
+    alt="Miguel Angel Ocampo"
+    className="w-[300px] h-[300px] rounded-full object-cover block"
+    loading="lazy"
+  />
+</div>
+    )
+}
